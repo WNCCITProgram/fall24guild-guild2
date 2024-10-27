a84ba0030cba7dd606d037732d09fecfae4f47c7
georgiajWNCC <161641740+georgiajWNCC@users.noreply.github.com>
2024-10-27 13:15:06 -0400

    Create week10_pseudo.py

---
+++ week10_pseudo.py
@@ -0,0 +1,40 @@
+# <code style="color : darkorange">10/27/24 Psuedo Code for Guild 2 Project Week 10</code>
+# <code style="color : darkorange">Description: Fuel service company framework</code><code style="color : darkorange">text</code>
+
+# <code style="color : darkorange">
+"""
+This Week 10 Milestone aimed to create the GUI framework to be used for the main program application.  
+It includes an example main application demonstration that utilizes the created gui framework.
+
+
+# -----> Week 10 Milestone Objectives:
+# Create GUI interface framework using tkinter
+# Establish menu interface 
+# Demonstrate use of GUI framework with main application
+# Research fuel management APIs and/or build fuel management database
+# Edit/modify week 9 writer package and created classes
+# Update main readme
+
+
+Create GUI interface framework
+# FuelManagementAppGUI class
+    # includes master, callback methods, and create widgets to set up gui components
+    # create widgets method
+    # tk.Label--> Name, address, phone number, email
+    # tk.Button--> add new customer, check fuel status
+
+    # Create add customer callback
+    # Get customer name, address, phone, email
+    # If-else statement message for callback success or failure
+
+    # Create check fuel status callback
+    # Call fuel status method
+
+
+
+
+Week 10 Stretch Goals:
+  Research and identify potential fuel management APIs to call.  If no suitable API's, begin to build fuel management database.
+
+
+"""
